60e022af480ee9b47419fdf809528676dc0553dc
Luis Perichon <luisperichon@gmail.com>
2013-08-14 13:39:45 -0300

    [#55103946] rename initializer

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,19 @@
+CarrierWave.configure do |config|
+  # on development & testing, upload files to local `tmp` folder.
+  if Rails.env.development? || Rails.env.test? || Rails.env.cucumber?
+    config.storage = :file
+    config.enable_processing = false
+    config.root = "#{Rails.root}/tmp"
+  else
+    config.storage = :fog
+    config.fog_credentials = {
+      # Configuration for Amazon S3
+      :provider => 'AWS',
+      :aws_access_key_id => ENV['S3_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET'],
+      :region => ENV['S3_REGION']
+    }
+    config.fog_directory = ENV['S3_BUCKET_NAME']
+    config.cache_dir = "#{Rails.root}/tmp/uploads" # To let CarrierWave work on heroku
+  end
+end
