42f93e9a4d587f310de9a92c2ba9502926baaa40
Luis Perichon <luisperichon@gmail.com>
2013-08-14 15:49:11 -0300

    remove region and use default

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,8 +10,7 @@ CarrierWave.configure do |config|
       # Configuration for Amazon S3
       :provider => 'AWS',
       :aws_access_key_id => ENV['S3_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET'],
-      :region => ENV['S3_REGION']
+      :aws_secret_access_key => ENV['S3_SECRET']
     }
     config.fog_directory = ENV['S3_BUCKET_NAME']
     config.cache_dir = "#{Rails.root}/tmp/uploads" # To let CarrierWave work on heroku
