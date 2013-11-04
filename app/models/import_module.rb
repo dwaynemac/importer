8f11387a4b9043126982a08490b378837f0795e0
Dwayne Macgowan <dwaynemac@gmail.com>
2013-11-04 13:08:33 -0300

    [#60031832]safer realtime_status

--- app/models/import_module.rb
+++ app/models/import_module.rb
@@ -3,7 +3,12 @@ class ImportModule < ActiveRecord::Base
 
   attr_accessible :name, :status, :status_url
 
+  # override in child class
+  def status_params
+  end
+
   def realtime_status
+    return nil if status_url.nil?
     unless self.status == 'finished'
       response = RestClient.get status_url, :params => status_params
       self.update_attributes(status: JSON.parse(response)['import']['status'])
--- spec/models/import_module_spec.rb
+++ spec/models/import_module_spec.rb
@@ -2,4 +2,13 @@ require 'spec_helper'
 
 describe ImportModule do
 
+  let(:import_module){create(:import_module)}
+
+  describe "#realtime_status" do
+    describe "if status_url if nil" do
+      it "returns nil" do
+        import_module.realtime_status.should be_nil
+      end
+    end
+  end
 end
