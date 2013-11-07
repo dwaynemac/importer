9a1f51bd593e74005cab06617615686ffb0d10eb
sebareca <sebareca@gmail.com>
2013-11-07 11:08:47 -0300

    [#60031832] change @import to self.import

--- app/models/contacts_import_module.rb
+++ app/models/contacts_import_module.rb
@@ -9,10 +9,10 @@ class ContactsImportModule < ImportModule
     # Import contacts csv to padma_contacts
     if Rails.env == 'development'
       # use local file path on development
-      contacts_csv = open(@import.import_file.contacts.path)
+      contacts_csv = open(self.import_file.contacts.path)
     else
       # use s3 file on production
-      contacts_csv = open(@import.import_file.contacts.url)
+      contacts_csv = open(self.import_file.contacts.url)
     end
 
 
@@ -20,7 +20,7 @@ class ContactsImportModule < ImportModule
     response = RestClient.post Contacts::HOST + '/v0/imports',
                     :app_key => Contacts::API_KEY,
                     :import => {
-                      :account_name => @import.account.name,
+                      :account_name => self.account.name,
                       :file => contacts_csv,
                       :headers => %w(id dni nombres apellidos dire tel cel mail grado_id instructor_id coeficiente_id genero foto
                         fecha_nacimiento inicio_practicas profesion	notes follow indice_fidelizacion codigo_postal school_id
