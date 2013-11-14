aa8ebc2d9e77efa93ad09b1509338efbe47d2db0
Alex Falke <afalkear@gmail.com>
2013-11-14 11:32:24 -0300

    [#60510220] communication importer spec

--- spec/factories/import_modules.rb
+++ spec/factories/import_modules.rb
@@ -4,4 +4,12 @@ FactoryGirl.define do
   factory :import_module do
     name 'contact'
   end
+  factory :communication_importer do
+    name 'communicationimporter'
+    import
+  end
+  factory :contacts_import_module do
+    name 'contactsimportmodule'
+    import
+  end
 end
---
+++ spec/models/communication_importer_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe CommunicationImporter do
+
+  let(:commimp) { create(:communication_importer) }
+  let(:cim) { create(:contacts_import_module, import: commimp.import) }
+
+  describe "#ready?" do
+    before do
+      #because let is lazy
+      cim
+    end
+
+    describe "when contact importer finished" do
+      before do
+        ContactsImportModule.any_instance.stub(:finished?) {true}
+      end
+      it "should be ready" do
+        commimp.ready?.should == true
+      end
+    end
+
+    describe "when contact importer has not finished" do
+      before do
+        ContactsImportModule.any_instance.stub(:finished?) {false}
+      end
+      it "shouldn't be ready" do
+        commimp.ready?.should == false
+      end
+    end
+
+  end
+
+  describe "#delegate_import" do
+    before do
+      commimp.stub_chain(:import_file, :time_slots, :url).and_return("x_path")
+      commimp.stub_chain(:open, :read) {"x_file"}
+    end
+
+    describe "if attendances-ws responds ok" do
+      before do
+        my_response = double("Response", code: 201, body: "{\"id\": \"1\"}")
+        RestClient.stub(:post).and_return(my_response)
+      end
+      it "should set status_url" do
+        commimp.delegate_import()
+        commimp.status_url.should == "test/api/v0/imports/1"
+      end
+    end
+  end
+
+end
