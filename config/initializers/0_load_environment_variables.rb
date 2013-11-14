438aa5ed8dd365651fa8d53df4aab9a15955e11e
Dwayne Macgowan <dwaynemac@gmail.com>
2013-11-14 11:18:26 -0300

    environment_variables.yml is not in travis-ci

--- config/initializers/0_load_environment_variables.rb
+++ config/initializers/0_load_environment_variables.rb
@@ -1,5 +1,6 @@
-if Rails.env.development? || Rails.env.test?
+if Rails.env.development?# || Rails.env.test?
   CONFIG = YAML.load_file("#{Rails.root}/config/environment_variables.yml")
 else
   CONFIG = {}
+  CONFIG['devise_secret_key'] = 'asdf'
 end
