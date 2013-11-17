33f0f76a4314d1546c26965fbe7908e1b99334b6
sebareca <sebareca@gmail.com>
2013-11-17 12:34:48 -0300

    [#60810524] Make status accessible, def realtime_status because status must be updated finished? modified to avoid querying

--- spec/models/import_spec.rb
+++ spec/models/import_spec.rb
@@ -6,20 +6,34 @@ describe Import do
   let(:cim) { create(:contacts_import_module, import: import) }
   let(:tsi) { create(:time_slot_importer, import: import) }
 
+  before do
+    cim
+    tsi
+  end
+
   describe "#finished?" do
-    before do
-      cim
-      tsi
-    end
-    it "is finished if all importers related to him are finished" do
+    it "has finished if all importers related to it had finished" do
       ContactsImportModule.any_instance.stub(:finished?).and_return(true)
       TimeSlotImporter.any_instance.stub(:finished?).and_return(true)
       import.finished?.should == true
     end
-    it "isn't finished if at least one importer related to him isn't finished" do
+    it "has not finished if at least one importer related to it had not finished" do
       ContactsImportModule.any_instance.stub(:finished?).and_return(true)
       TimeSlotImporter.any_instance.stub(:finished?).and_return(false)
       import.finished?.should == false
+    end
+  end
+
+  describe "#realtime_status" do
+    it "should make a query if it has not finished" do
+      #require 'byebug'; byebug
+      ContactsImportModule.any_instance.should_receive(:finished?).once
+      import.realtime_status
+    end
+    it "shouldn't make a query if it has finished" do
+      import.update_attribute(:status, 'finished')
+      ContactsImportModule.any_instance.should_not_receive(:finished?)
+      import.realtime_status
     end
   end
 
