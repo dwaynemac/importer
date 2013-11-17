4aa2709c3bc1afdb83ae2302e6118a2f91c4b618
sebareca <sebareca@gmail.com>
2013-11-17 12:32:18 -0300

    [#60810524] Add status attribute to Import

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131104150113) do
+ActiveRecord::Schema.define(version: 20131115212419) do
 
   create_table "accounts", force: true do |t|
     t.string   "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20131104150113) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "import_file"
+    t.string   "status",      default: "pending"
   end
 
   create_table "users", force: true do |t|
