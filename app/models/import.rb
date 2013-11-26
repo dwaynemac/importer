495255a0782c356b246c951e422a513d7ee329ca
sebareca <sebareca@gmail.com>
2013-11-26 13:26:14 -0300

    [#61157040] Import status logic + Specs

--- app/models/import.rb
+++ app/models/import.rb
@@ -19,11 +19,22 @@ class Import < ActiveRecord::Base
       importer.process
     end
   end
-
+    
+  
+  # First we check if status is finished of failed because there's no turning back
+  # of that status
+  # If at least one has failed the overall status is failed
+  # If all have finished then the overall status is finished
+  # Finnaly there are only two statuses left working and ready, if one of them is not ready
+  # then it must be working
   def realtime_status 
-    return status if status == 'finished'
-    if import_modules.load.select{ |im| not im.finished? }.count == 0
+    return status if status == 'finished' or status == 'failed'
+    if import_modules.load.select{ |im| im.status == 'failed'}.count > 0
+      update_attribute(:status, 'failed')
+    elsif import_modules.load.select{ |im| im.finished? }.count == import_modules.size
       update_attribute(:status, 'finished')
+    elsif import_modules.load.select{ |im| im.status != 'ready' }.count > 0 and status != 'working'
+      update_attribute(:status, 'working')
     end
     status
   end
