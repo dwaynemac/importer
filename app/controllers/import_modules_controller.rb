d68d70e4180ec45b475b292eba4d524fbed36687
¨sebareca¨ <sebareca@gmail.com>
2014-01-06 13:53:57 -0300

    [#62896842] failed_files controller
    1) get failed_contacts_ids
    2) start new update
    3) change status_url

--- app/controllers/import_modules_controller.rb
+++ app/controllers/import_modules_controller.rb
@@ -17,6 +17,26 @@ class ImportModulesController < ApplicationController
     end
   end
 
+  def failed_files
+    im = ImportModule.find params[:id]
+    failed_contacts_response = RestClient.get "#{im.status_url}/failed_contacts",
+                                              params: im.status_params
+    
+    failed_contacts_ids = JSON.parse(failed_contacts_response)[:failed_contacts]
+
+    response = RestClient.post  Kshema::HOST + '/pws/v1/files_export',
+                                key: Kshema::API_KEY,
+                                account_name: im.import.account.name,
+                                contacts_ids: failed_contacts_ids
+    if response.code == 201
+      im.update_attributes(status_url: Kshema::HOST + '/pws/v1/files_export/' + JSON.parse(response)['id'])
+    end
+
+    respond_to do |format|
+      format.html { redirect_to im.import }
+    end
+  end
+
   private
 
   def import_module_params
