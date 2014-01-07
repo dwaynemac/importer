77e88e379ea10a68ed5228297a7ec42b1ad4b17a
¨sebareca¨ <sebareca@gmail.com>
2014-01-07 15:07:28 -0300

    [#62896842] parse body not response

--- app/models/contacts_file_importer.rb
+++ app/models/contacts_file_importer.rb
@@ -10,8 +10,8 @@ class ContactsFileImporter < ImportModule
                                 account_name: self.import.account.name
 
     if(response.code == 201)
-      remote_import_id = JSON.parse(response)['id']
-      self.update_attributes(status_url: Kshema::HOST + '/pws/v1/files_export/' + remote_import_id)
+      remote_import_id = JSON.parse(response.body)['id']
+      self.update_attributes(status_url: Kshema::HOST + '/pws/v1/files_export/' + remote_import_id.to_s)
     end
   end
   
