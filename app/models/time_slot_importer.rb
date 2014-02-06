8e29a4b307064e7e01d08424d4ec7f1879f30827
Dwayne Macgowan <dwaynemac@gmail.com>
2014-02-06 17:14:38 -0300

    if module fails with exception response is not a valid json

--- app/models/time_slot_importer.rb
+++ app/models/time_slot_importer.rb
@@ -63,7 +63,11 @@ class TimeSlotImporter < ImportModule
       self.update_attributes(status_url: Attendance::HOST + '/api/v0/imports/' + remote_import_id.to_s)
     end
   rescue RestClient::Exception => e
-    response_body = JSON.parse(e.response)
+    response_body = begin
+      JSON.parse(e.response)
+    rescue
+      e.response
+    end
     Rails.logger.warn "#{self.type}##{self.id} #{response_body['error']}"
   end
 
