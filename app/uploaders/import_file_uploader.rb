d9cabefdbfd39cc9b57c1991c0eccd65d147b6fb
Alex Falke <afalkear@gmail.com>
2014-03-18 12:28:13 -0300

    [#66800784] upload separate files for kshema and sys

--- app/uploaders/import_file_uploader.rb
+++ app/uploaders/import_file_uploader.rb
@@ -7,98 +7,98 @@ class ImportFileUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  version :contacts do
-    process :extract_file => :personas
+  version :contacts, :if => :is_kshema? do
+    process :extract_file_from_kshema_zip => :personas
     def full_filename (for_file = model.import_file.file)
       "contacts.csv"
     end
   end
 
-  version :time_slots do
-    process :extract_file => :horarios
+  version :time_slots, :if => :is_kshema? do
+    process :extract_file_from_kshema_zip => :horarios
     def full_filename (for_file = model.import_file.file)
       "time_slots.csv"
     end
   end
 
-  version :attendances do
-    process :extract_file => :asistencias
+  version :attendances, :if => :is_kshema? do
+    process :extract_file_from_kshema_zip => :asistencias
     def full_filename (for_file = model.import_file.file)
       "attendances.csv"
     end
   end
   
-  version :trial_lessons do
-    process :extract_file => :pruebas
+  version :trial_lessons, :if => :is_kshema? do
+    process :extract_file_from_kshema_zip => :pruebas
     def full_filename (for_file = model.import_file.file)
       "trial_lessons.csv"
     end
   end
 
-  version :products do
-    process :extract_file => :productos
+  version :products, :if => :is_kshema? do
+    process :extract_file_from_kshema_zip => :productos
     def full_filename (for_file = model.import_file.file)
       "products.csv"
     end
   end
 
-  version :sales do
-    process :extract_file => :ventas
+  version :sales, :if => :is_kshema? do
+    process :extract_file_from_kshema_zip => :ventas
     def full_filename (for_file = model.import_file.file)
       "sales.csv"
     end
   end
 
-  version :memberships do
-    process :extract_file => :plans
+  version :memberships, :if => :is_kshema? do
+    process :extract_file_from_kshema_zip => :plans
     def full_filename (for_file = model.import_file.file)
       "memberships.csv"
     end
   end
 
-  version :installments do
-    process :extract_file => :tickets
+  version :installments, :if => :is_kshema? do
+    process :extract_file_from_kshema_zip => :tickets
     def full_filename (for_file = model.import_file.file)
       "installments.csv"
     end
   end
   
-  version :comments do
-    process :extract_file => :follow_ups
+  version :comments, :if => :is_kshema? do
+    process :extract_file_from_kshema_zip => :follow_ups
     def full_filename (for_file = model.import_file.file)
       "comments.csv"
     end
   end
   
-  version :communications do
-    process :extract_file => :contacts
+  version :communications, :if => :is_kshema? do
+    process :extract_file_from_kshema_zip => :contacts
     def full_filename (for_file = model.import_file.file)
       "communications.csv"
     end
   end
   
-  version :drop_outs do
-    process :extract_file => :evasions
+  version :drop_outs, :if => :is_kshema? do
+    process :extract_file_from_kshema_zip => :evasions
     def full_filename (for_file = model.import_file.file)
       "drop_outs.csv"
     end
   end
 
-  version :enrollments do
-    process :extract_file => :matriculas
+  version :enrollments, :if => :is_kshema? do
+    process :extract_file_from_kshema_zip => :matriculas
     def full_filename (for_file = model.import_file.file)
       "enrollments.csv"
     end
   end
 
-  version :follows do
-    process :extract_file => :instructors
+  version :follows, :if => :is_kshema? do
+    process :extract_file_from_kshema_zip => :instructors
     def full_filename (for_file = model.import_file.file)
       "follows.csv"
     end
   end
 
-  def extract_file(filename)
+  def extract_file_from_kshema_zip(filename)
     file = nil
     Zip::ZipFile.open(current_path) do |zip_file|
       file = zip_file.select{|f| f.name.match(/.*#{filename}/)}.first
@@ -107,4 +107,9 @@ class ImportFileUploader < CarrierWave::Uploader::Base
     File.delete(current_path)
     FileUtils.cp("tmp/" + file.name.gsub("/", "-"), current_path)
   end
+
+  protected
+   def is_kshema? file
+    model.source_system == 'kshema'
+   end
 end
