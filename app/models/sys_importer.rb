6ae505396ba3aed5b4a9cdccbc444eb9c9ceacde
Alex Falke <afalkear@gmail.com>
2014-03-21 21:16:13 -0300

    [#66800784] file uploader versions for SYS

---
+++ app/models/sys_importer.rb
@@ -0,0 +1,35 @@
+class SysImporter
+  attr_accessor :import
+
+  def initialize(import)
+    @import = import
+  end
+
+  # delegations will be done on background
+  def process
+    proccess_contacts_imports
+    #proccess_attendance_imports
+    #proccess_crm_imports
+  end
+
+  private
+
+  def proccess_attendance_imports
+    TimeSlotImporter.create(import: @import)
+    AttendanceImporter.create(import: @import)
+    TrialLessonImporter.create(import: @import)
+  end
+
+  def proccess_contacts_imports
+    ContactsImportModule.create(import: @import)
+  end
+
+  def proccess_crm_imports
+    CommentImporter.create(import: @import)
+    CommunicationImporter.create(import: @import)
+    FollowImporter.create(import: @import)
+    EnrollmentImporter.create(import: @import)
+    DropoutImporter.create(import: @import)
+  end
+
+end
--- app/uploaders/import_file_uploader.rb
+++ app/uploaders/import_file_uploader.rb
@@ -1,4 +1,5 @@
 # encoding: utf-8
+require 'csv'
 
 class ImportFileUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
@@ -7,94 +8,110 @@ class ImportFileUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  version :contacts, :if => :is_kshema? do
-    process :extract_file_from_kshema_zip => :personas
+  ## Kshêma versions
+
+  version :contacts do
+    process :extract_file => :personas
     def full_filename (for_file = model.import_file.file)
       "contacts.csv"
     end
   end
 
-  version :time_slots, :if => :is_kshema? do
-    process :extract_file_from_kshema_zip => :horarios
+  version :time_slots do
+    process :extract_file => :horarios
     def full_filename (for_file = model.import_file.file)
       "time_slots.csv"
     end
   end
 
-  version :attendances, :if => :is_kshema? do
-    process :extract_file_from_kshema_zip => :asistencias
+  version :attendances do
+    process :extract_file => :asistencias
     def full_filename (for_file = model.import_file.file)
       "attendances.csv"
     end
   end
   
-  version :trial_lessons, :if => :is_kshema? do
-    process :extract_file_from_kshema_zip => :pruebas
+  version :trial_lessons do
+    process :extract_file => :pruebas
     def full_filename (for_file = model.import_file.file)
       "trial_lessons.csv"
     end
   end
 
-  version :products, :if => :is_kshema? do
-    process :extract_file_from_kshema_zip => :productos
+  version :products do
+    process :extract_file => :productos
     def full_filename (for_file = model.import_file.file)
       "products.csv"
     end
   end
 
-  version :sales, :if => :is_kshema? do
-    process :extract_file_from_kshema_zip => :ventas
+  version :sales do
+    process :extract_file => :ventas
     def full_filename (for_file = model.import_file.file)
       "sales.csv"
     end
   end
 
-  version :memberships, :if => :is_kshema? do
-    process :extract_file_from_kshema_zip => :plans
+  version :memberships do
+    process :extract_file => :plans
     def full_filename (for_file = model.import_file.file)
       "memberships.csv"
     end
   end
 
-  version :installments, :if => :is_kshema? do
-    process :extract_file_from_kshema_zip => :tickets
+  version :installments do
+    process :extract_file => :tickets
     def full_filename (for_file = model.import_file.file)
       "installments.csv"
     end
   end
   
-  version :comments, :if => :is_kshema? do
-    process :extract_file_from_kshema_zip => :follow_ups
+  version :comments do
+    process :extract_file => :follow_ups
     def full_filename (for_file = model.import_file.file)
       "comments.csv"
     end
   end
   
-  version :communications, :if => :is_kshema? do
-    process :extract_file_from_kshema_zip => :contacts
+  version :communications do
+    process :extract_file => :contacts
     def full_filename (for_file = model.import_file.file)
       "communications.csv"
     end
   end
   
-  version :drop_outs, :if => :is_kshema? do
-    process :extract_file_from_kshema_zip => :evasions
+  version :drop_outs do
+    process :extract_file => :evasions
     def full_filename (for_file = model.import_file.file)
       "drop_outs.csv"
     end
   end
 
-  version :enrollments, :if => :is_kshema? do
-    process :extract_file_from_kshema_zip => :matriculas
+  version :enrollments do
+    process :extract_file => :matriculas
     def full_filename (for_file = model.import_file.file)
       "enrollments.csv"
     end
   end
 
-  version :follows, :if => :is_kshema? do
-    process :extract_file_from_kshema_zip => :instructors
+  version :follows do
+    process :extract_file => :instructors
     def full_filename (for_file = model.import_file.file)
       "follows.csv"
+    end
+  end
+
+  ## SYS versions
+
+  def extract_file(filename)
+    file = nil
+    case model.source_system
+      when 'kshema'
+        file = extract_file_from_kshema_zip filename
+      when 'sys'
+        if [:personas].include? filename
+          file = extract_file_from_sys_csv filename 
+        end
     end
   end
 
@@ -108,8 +125,35 @@ class ImportFileUploader < CarrierWave::Uploader::Base
     FileUtils.cp("tmp/" + file.name.gsub("/", "-"), current_path)
   end
 
+  def extract_file_from_sys_csv(model_name)
+    file = send("generate_sys_#{model_name}_file")
+    File.delete(current_path)
+    FileUtils.cp(file.path, current_path)
+  end
+
+  def generate_sys_personas_file
+    headers = %w(NumeroCliente Apelido BairroRes BairroCom Cargo Categoria CEP CEPCom Cidade CidadeCom 
+      Email Empresa EndResidencial EndProfissional EndComplRes EndComplCom NumRes NumCom 
+      Estado EstadoCom EstadoCivil Grau IndicadoPor Livros_que_leu Nascimento Nome Perfil
+      Profissao Observacoes sexo Telefone2 TelefoneRes TelefoneCom)
+    
+    
+    CSV.open("tmp/contacts.csv","w") do |csv|
+      csv << headers
+      CSV.foreach(current_path, col_sep: ";", encoding: "UTF-8", headers: :first_row) do |row|
+        current_row = []
+        headers.each do |h|
+          current_row << get_value_for(h, row, headers)
+        end
+        csv << current_row
+      end
+    end
+    File.open("tmp/contacts.csv","r")
+  end
+
   protected
-   def is_kshema? file
-    model.source_system == 'kshema'
-   end
+
+   def get_value_for(field, row, headers)
+      headers.index(field).nil? ? nil : row[headers.index(field)]
+    end
 end
