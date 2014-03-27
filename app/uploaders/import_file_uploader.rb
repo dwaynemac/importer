0c78a8d6015d111cd6801bf5e2fd8334b1250c25
Alex Falke <afalkear@gmail.com>
2014-03-27 13:39:31 -0300

    [#66800784] generate SYS personas file

--- app/uploaders/import_file_uploader.rb
+++ app/uploaders/import_file_uploader.rb
@@ -109,7 +109,7 @@ class ImportFileUploader < CarrierWave::Uploader::Base
       when 'kshema'
         file = extract_file_from_kshema_zip filename
       when 'sys'
-        if [:personas].include? filename
+        if [:personas, :contacts, :evasions, :matriculas, :horarios].include? filename
           file = extract_file_from_sys_csv filename 
         end
     end
@@ -132,18 +132,52 @@ class ImportFileUploader < CarrierWave::Uploader::Base
   end
 
   def generate_sys_personas_file
-    headers = %w(NumeroCliente Apelido BairroRes BairroCom Cargo Categoria CEP CEPCom Cidade CidadeCom 
-      Email Empresa EndResidencial EndProfissional EndComplRes EndComplCom NumRes NumCom 
-      Estado EstadoCom EstadoCivil Grau IndicadoPor Livros_que_leu Nascimento Nome Perfil
-      Profissao Observacoes sexo Telefone2 TelefoneRes TelefoneCom)
+    headers = %w(NumeroCliente Apelido BairroRes BairroCom Cargo Categoria CEP Cidade 
+      Email Empresa EndResidencial Estado EstadoCivil Grau IndicadoPor Livros_que_leu Nascimento Nome 
+      Profissao Observacoes sexo Telefone2 TelefoneRes TelefoneCom Ja_praticou Ja_similar DataGrad)
+    padma_headers = %w(id apelido state commercial_address cargo coeficiente_id codigo_postal city mail 
+      company dire state civil_state grado_id indicado_por livros_que_leu fecha_nacimiento nombres 
+      apellidos profesion notes genero tel other_tel tel_com ja_practicou ja_similar data_grad) 
     
-    
+    # TODO change values like sexo to match padma values
     CSV.open("tmp/contacts.csv","w") do |csv|
-      csv << headers
+      csv << padma_headers
       CSV.foreach(current_path, col_sep: ";", encoding: "UTF-8", headers: :first_row) do |row|
         current_row = []
+        complete_headers = row.headers()
         headers.each do |h|
-          current_row << get_value_for(h, row, headers)
+          value = get_value_for(h, row, complete_headers)
+          case h
+            when 'NumeroCliente'
+              value = set_id_from_account(value, model.account.name)
+            when 'EndResidencial'
+              value = generate_address(
+                value, get_value_for('NumRes', row, complete_headers),
+                get_value_for('EndComplRes', row, complete_headers)
+                )
+            when 'BairroCom'
+              value = generate_commercial_address(
+                value, 
+                get_value_for('CEPCom', row, complete_headers),
+                get_value_for('CidadeCom', row, headers),
+                get_value_for('EndProfissional', row, complete_headers),
+                get_value_for('EndComplCom', row, complete_headers),
+                get_value_for('NumCom', row, complete_headers),
+                get_value_for('EstadoCom', row, complete_headers)
+                )
+            when 'Categoria'
+              value = get_valid_contacts_coefficient(value, get_value_for('Perfil',row, complete_headers))
+            when 'Grau'
+              value = get_valid_contacts_level(value)
+            when 'Nome'
+              names = set_name_and_last_name(value)
+              current_row << names[:nombres]
+              value = names[:apellidos]
+            when 'sexo'
+              value = get_valid_contacts_gender(value)
+          end
+
+          current_row << value
         end
         csv << current_row
       end
@@ -151,9 +185,179 @@ class ImportFileUploader < CarrierWave::Uploader::Base
     File.open("tmp/contacts.csv","r")
   end
 
+  def generate_sys_contacts_file
+    headers = %w(NumeroCliente DataTelefone DataVisita DataEmail Motivo)
+
+    # TODO change values like sexo to match padma values
+    # Make two rows if there is a call and a visit
+    CSV.open("tmp/communications.csv","w") do |csv|
+      csv << headers
+      CSV.foreach(current_path, col_sep: ";", encoding: "UTF-8", headers: :first_row) do |row|
+        complete_headers = row.headers()
+        current_row = []
+        headers.each do |h|
+          current_row << get_value_for(h, row, complete_headers)
+        end
+        csv << current_row
+      end
+    end
+    File.open("tmp/communications.csv","r")
+  end
+
+  def generate_sys_evasions_file
+    headers = %w(NumeroCliente DataSaida Motivo)
+
+    # TODO change values like sexo to match padma values
+    # Make two rows if there is a call and a visit
+    CSV.open("tmp/drop_outs.csv","w") do |csv|
+      csv << headers
+      CSV.foreach(current_path, col_sep: ";", encoding: "UTF-8", headers: :first_row) do |row|
+        complete_headers = row.headers()
+        current_row = []
+        headers.each do |h|
+          current_row << get_value_for(h, row, complete_headers)
+        end
+        csv << current_row
+      end
+    end
+    File.open("tmp/drop_outs.csv","r")
+  end
+
+  def generate_sys_matriculas_file
+    headers = %w(NumeroCliente DataMatricula)
+
+    # TODO change values like sexo to match padma values
+    # Make two rows if there is a call and a visit
+    CSV.open("tmp/enrollments.csv","w") do |csv|
+      csv << headers
+      CSV.foreach(current_path, col_sep: ";", encoding: "UTF-8", headers: :first_row) do |row|
+        complete_headers = row.headers()
+        current_row = []
+        headers.each do |h|
+          current_row << get_value_for(h, row, complete_headers)
+        end
+        csv << current_row
+      end
+    end
+    File.open("tmp/enrollments.csv","r")
+  end
+
+  def generate_sys_horarios_file
+    headers = %w(Turma Yoga_Bio)
+    # TODO change values like sexo to match padma values
+    # Make two rows if there is a call and a visit
+    CSV.open("tmp/time_slots.csv","w") do |csv|
+      csv << headers
+      CSV.foreach(current_path, col_sep: ";", encoding: "UTF-8", headers: :first_row) do |row|
+        complete_headers = row.headers()
+        current_row = []
+        headers.each do |h|
+          current_row << get_value_for(h, row, complete_headers)
+        end
+        csv << current_row
+      end
+    end
+    File.open("tmp/time_slots.csv","r")
+  end
+
   protected
 
-   def get_value_for(field, row, headers)
-      headers.index(field).nil? ? nil : row[headers.index(field)]
+   def set_id_from_account(id, account_name)
+    return account_name+id
+   end
+
+   def get_value_for(field, row, complete_headers)
+      response = complete_headers.index(field).nil? ? nil : row[complete_headers.index(field)]
+      clean_value(response)
+   end
+
+   def generate_commercial_address(barrio, cep, city, address, com_number, number, state)
+    response = [barrio, cep, city, address, com_number, number, state].reject!(&:blank?)
+    if !response.nil?
+      response = response.join(',')
     end
+    return response
+   end
+
+   def generate_address(address, number, end_compl_res)
+    response = address.to_s
+    response << "num: #{number}" unless number.blank?
+    response << "end_compl_res: #{end_compl_res}" unless end_compl_res.blank?      
+
+    return response
+   end
+
+   def set_name_and_last_name(value)
+      name = value.split.first
+      last_name = value.split[1..10].join(" ")
+     {:nombres => name, :apellidos => last_name}
+   end
+
+   def get_valid_contacts_gender(value) 
+     if %w(fem a).include? value
+      return 'female'
+    elsif %w(mas o)
+      return 'male'
+    else
+      return nil
+    end
+   end
+
+   def get_valid_contacts_level(value)
+     case value.try :downcase
+      when 'aspirante'
+        return 'aspirante'
+      when'assistente'
+        return 'asistente'
+      when 'chêla'
+        return 'chêla'
+      when 'graduado'
+        return 'graduado'
+      when 'NULL'
+        return ''
+      when 'sádhaka'
+        return 'sádhaka'
+      when 'yôgin'
+        return 'yôgin'
+     end
+   end
+
+   def get_valid_contacts_coefficient(status, coefficient)
+    case status.try :downcase
+      when 'aluno'
+        return 'alumno'
+      when 'ex-aluno'
+        if is_perfil?(coefficient)
+          return 'exalumno'
+        else
+          return 'exalumnofp'
+        end
+      when 'visita'
+        if is_perfil?(coefficient)
+          return 'perfil'
+        else
+          return 'fp'
+        end
+      when 'telefone'
+        return 'unknown'
+      when 'site'
+        return 'unknown'
+      when 'equipe'
+        return 'unknown'
+      when 'fest-Yôga'
+        return 'unknown'
+      when 'profissional'
+        return 'unknown'
+      else
+        return 'unknown'
+    end
+   end
+
+   def is_perfil?(value)
+     return %w(s sim S NULL Sim).include? value || value.blank?
+   end
+
+   def clean_value(value)
+    return (value == 'NULL' ? '' : value)
+   end
 end
